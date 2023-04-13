9a638f1c615aeae716c9274e54b9539d76f31ef3
Youssef Abdulaziz <azizy5203@gmail.com>
2023-04-13 03:16:59 +0200

    In-place pricing contenteditable

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,51 @@
 <script setup>
+import { ref, computed } from "vue";
 import SimpleList from "./components/SimpleList.vue";
 import Store from "./components/Store.vue";
+
+const left = ref(400);
+const mid = ref(5.564);
+const right = computed(() => {
+  return left.value - left.value * (mid.value / 100).toFixed(4);
+});
+const viewer = ref("ddd");
+
+function handleLeftChange(e) {
+  viewer.value = e.target.textContent;
+  left.value = +e.target.textContent;
+}
+function handleMidChange(e) {
+  viewer.value = e.target.textContent;
+  mid.value = +e.target.textContent;
+}
 </script>
 
 <template>
   <!-- <SimpleList /> -->
-  <Store />
+  <!-- <Store /> -->
+  <p class="viewer">
+    {{ viewer }}
+  </p>
+  <div class="grid">
+    <p contenteditable @input="handleLeftChange($event)">{{ left }}</p>
+    <p contenteditable @input="handleMidChange($event)">{{ mid }}</p>
+    <p contenteditable>{{ right }}</p>
+  </div>
 </template>
 
-<style scoped></style>
+<style lang="scss" scoped>
+.viewer {
+  background-color: black;
+  font-size: xx-large;
+}
+.grid {
+  display: grid;
+  grid-template-columns: repeat(3, auto);
+  background-color: beige;
+  gap: 1rem;
+  p {
+    background-color: dodgerblue;
+    font-size: xx-large;
+  }
+}
+</style>
